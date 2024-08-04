ac6540471f8d0d5572f1fbc7aad4995ca2357e28
manthan127 <gevariyamanthan8@gmail.com>
2024-08-04 15:31:39 +0530

    added example project

---
+++ Example/UtilTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UtilTest
+//
+//  Created by Home on 04/08/24.
+//
+
+import UIKit
+import Utils
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        CGPointZero + CGPoint()
+    }
+}
