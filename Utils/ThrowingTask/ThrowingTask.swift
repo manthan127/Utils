aaadd11f2c7548236c8b6dcaad0e3328970f6cfa
manthan127 <gevariyamanthan8@gmail.com>
2025-05-24 18:08:03 +0530

    throwing task in View

--- Utils/ThrowingTask/ThrowingTask.swift
+++ Utils/ThrowingTask/ThrowingTask.swift
@@ -6,13 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+typealias throwingTaskResult<Success> = Task<Result<Success, Error>, Never>
 
 @discardableResult
 func ThrowingTask<Success>(
     priority: TaskPriority? = nil,
     operation: @escaping @Sendable () async throws -> Success,
     errorHandler: @escaping (Error)->Void
-) -> Task<Result<Success, Error>, Never> {
+) -> throwingTaskResult<Success> {
     Task(priority: priority) {
         do {
             return .success(try await operation())
@@ -28,7 +31,7 @@ func ThrowingTaskDetached<Success>(
     priority: TaskPriority? = nil,
     operation: @escaping @Sendable () async throws -> Success,
     errorHandler: @escaping (Error) -> Void
-) -> Task<Result<Success, Error>, Never> {
+) -> throwingTaskResult<Success> {
     Task.detached(priority: priority) {
         do {
             return .success(try await operation())
@@ -45,7 +48,7 @@ func ThrowingTask<Success>(
     operation: @escaping @Sendable () async throws -> Success,
     typedHandlers: (any AnyErrorHandler)...,
     defaultHandler: @escaping (Error)->Void
-) -> Task<Result<Success, Error>, Never> {
+) -> throwingTaskResult<Success> {
     Task(priority: priority) {
         do {
             return .success(try await operation())
@@ -68,7 +71,7 @@ func ThrowingTaskDetached<Success>(
     operation: @escaping @Sendable () async throws -> Success,
     typedHandlers: (any AnyErrorHandler)...,
     defaultHandler: @escaping (Error)->Void
-) -> Task<Result<Success, Error>, Never> {
+) -> throwingTaskResult<Success> {
     Task.detached(priority: priority) {
         do {
             return .success(try await operation())
@@ -81,6 +84,43 @@ func ThrowingTaskDetached<Success>(
             
             defaultHandler(error)
             return .failure(error)
+        }
+    }
+}
+
+
+extension View {
+    func throwingTask(
+        priority: TaskPriority = .userInitiated,
+        _ action: @escaping @Sendable () async throws -> Void,
+        errorHandler: @escaping (Error)->Void
+    ) -> some View {
+        self.task(priority: priority) {
+            do {
+                try await action()
+            } catch {
+                errorHandler(error)
+            }
+        }
+    }
+    
+    func throwingTask(
+        priority: TaskPriority = .userInitiated,
+        _ action: @escaping @Sendable () async throws -> Void,
+        typedHandlers: (any AnyErrorHandler)...,
+        errorHandler: @escaping (Error)->Void
+    ) -> some View {
+        self.task(priority: priority) {
+            do {
+                try await action()
+            } catch {
+                for errorHandler in typedHandlers {
+                    if errorHandler.handle(error) {
+                        return
+                    }
+                }
+                errorHandler(error)
+            }
         }
     }
 }
