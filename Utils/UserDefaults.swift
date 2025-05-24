fa9187fc5e330ee1fc8a79e9517cb5390f92452f
manthan127 <gevariyamanthan8@gmail.com>
2025-05-24 18:34:19 +0530

    userdefault code update

--- Utils/UserDefaults.swift
+++ Utils/UserDefaults.swift
@@ -7,36 +7,53 @@
 
 import Foundation
 
+struct UserDefaultsKeys: RawRepresentable {
+    init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+    var rawValue: String
+
+}
+
 extension UserDefaults {
-    private func setValue<E: Encodable>(_ object: E?, forKey key: String, _ onError: ((Error)->())? = nil) {
+    private func setValue<E: Encodable>(_ object: E?, forKey key: UserDefaultsKeys, _ onError: ((Error)->())?) {
         if object == nil {
-            return removeObject(forKey: key)
+            return removeObject(forKey: key.rawValue)
         }
-        
         do {
             let data = try JSONEncoder().encode(object)
-            self.set(data, forKey: key)
+            self.set(data, forKey: key.rawValue)
         } catch {
             onError?(error)
         }
     }
 
-    private func object<D: Decodable>(forType type: D.Type, forKey key: String, _ onError: ((Error)->())? = nil)-> D? {
-        guard let data = self.data(forKey: key) else {return nil}
+    private func object<D: Decodable>(forType type: D.Type, forKey key: UserDefaultsKeys) throws -> D? {
+        guard let data = self.data(forKey: key.rawValue) else {return nil}
+        
+        return try JSONDecoder().decode(D.self, from: data)
+    }
+    
+    private func object<D: Decodable>(forType type: D.Type, forKey key: UserDefaultsKeys, _ onError: ((Error)->())?)-> D? {
         do {
-            return try JSONDecoder().decode(D.self, from: data)
+            return try object(forType: D.self, forKey: key)
         } catch {
             onError?(error)
             return nil
         }
     }
     
-    public subscript<E: Codable>(key: String, onError: ((Error)->())? = nil) -> E? {
+    subscript<E: Codable>(key: UserDefaultsKeys, onError: ((Error)->())? = nil) -> E? {
         get {
             object(forType: E.self, forKey: key, onError)
         } set {
             setValue(newValue, forKey: key, onError)
         }
     }
+    
+    subscript<E: Codable>(key: UserDefaultsKeys) -> E? {
+        get throws {
+            try object(forType: E.self, forKey: key)
+        }
+    }
 }
-
